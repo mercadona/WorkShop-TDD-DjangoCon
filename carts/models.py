2cf573e55a7f21539a629671c777c804906439df
Luis Laguna <lulaguna@mercadona.es>
2023-05-25 13:09:44 +0200

    ref: Rename sku as reference
    For be more understable for people not involved on retail

--- carts/models.py
+++ carts/models.py
@@ -4,9 +4,9 @@ from django.core.validators import MinValueValidator
 
 class CartLine(models.Model):
     # This field is unique for this workshop purposes hehe :)
-    sku = models.CharField(max_length=5, unique=True)
+    reference = models.CharField(max_length=5, unique=True)
     quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
 
     # In a normal project this would not go here
     def to_dict(self):
-        return {"sku": self.sku, "quantity": self.quantity}
+        return {"reference": self.reference, "quantity": self.quantity}
--- carts/tests.py
+++ carts/tests.py
@@ -13,44 +13,44 @@ def test_returns_empty_cart(client):
 
 
 def test_returns_cart_with_one_product(client):
-    CartLine.objects.create(sku="50776", quantity=5)
+    CartLine.objects.create(reference="50776", quantity=5)
 
     response = client.get("/cart/")
 
     assert response.status_code == 200
-    assert response.json() == {"lines": [{"sku": "50776", "quantity": 5}]}
+    assert response.json() == {"lines": [{"reference": "50776", "quantity": 5}]}
 
 
 def test_adds_product_to_cart_when_product_not_in_cart(client):
     # Arrange (?)
-    # Act (post a quantity to any sku)
+    # Act (post a quantity to any reference)
     # Assert (check status_code and response of the new cart state)
     pass
 
 
 def test_adds_product_units_to_existing_product_in_cart(client):
-    CartLine.objects.create(sku="50776", quantity=5)
-    # Act (post a quantity to the 50776 sku)
+    CartLine.objects.create(reference="50776", quantity=5)
+    # Act (post a quantity to the 50776 reference)
     # Assert (check status_code and response of the new cart state)
     pass
 
 
 def test_substracts_quantity(client):
-    CartLine.objects.create(sku="50776", quantity=5)
-    # Act (post a quantity substraction to the 50776 sku)
+    CartLine.objects.create(reference="50776", quantity=5)
+    # Act (post a quantity substraction to the 50776 reference)
     # Assert (check status_code and response of the new cart state)
     pass
 
 
 def test_removes_product_when_substracting_greater_quantity_than_the_carts_has(client):
-    CartLine.objects.create(sku="50776", quantity=5)
-    # Act (substract more than 5 units to the 50776 sku)
+    CartLine.objects.create(reference="50776", quantity=5)
+    # Act (substract more than 5 units to the 50776 reference)
     # Assert (check status_code and response of the new cart state)
     pass
 
 
 def test_does_nothing_when_product_does_not_exists(client):
     # Arrange (?)
-    # Act (substract any quantity to any sku)
+    # Act (substract any quantity to any reference)
     # Assert (check status_code and response of the new cart state)
     pass
