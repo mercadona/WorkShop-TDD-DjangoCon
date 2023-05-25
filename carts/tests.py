ac52c5b5f7d78e67702f33382d3c500f0fcf36c8
Luis Laguna <lulaguna@mercadona.es>
2023-05-25 12:51:23 +0200

    del: Remove pytest.skip

--- carts/tests.py
+++ carts/tests.py
@@ -21,7 +21,6 @@ def test_returns_cart_with_one_product(client):
     assert response.json() == {"lines": [{"sku": "50776", "quantity": 5}]}
 
 
-@pytest.mark.skip
 def test_adds_product_to_cart_when_product_not_in_cart(client):
     # Arrange (?)
     # Act (post a quantity to any sku)
@@ -29,7 +28,6 @@ def test_adds_product_to_cart_when_product_not_in_cart(client):
     pass
 
 
-@pytest.mark.skip
 def test_adds_product_units_to_existing_product_in_cart(client):
     CartLine.objects.create(sku="50776", quantity=5)
     # Act (post a quantity to the 50776 sku)
@@ -37,7 +35,6 @@ def test_adds_product_units_to_existing_product_in_cart(client):
     pass
 
 
-@pytest.mark.skip
 def test_substracts_quantity(client):
     CartLine.objects.create(sku="50776", quantity=5)
     # Act (post a quantity substraction to the 50776 sku)
@@ -45,7 +42,6 @@ def test_substracts_quantity(client):
     pass
 
 
-@pytest.mark.skip
 def test_removes_product_when_substracting_greater_quantity_than_the_carts_has(client):
     CartLine.objects.create(sku="50776", quantity=5)
     # Act (substract more than 5 units to the 50776 sku)
@@ -53,7 +49,6 @@ def test_removes_product_when_substracting_greater_quantity_than_the_carts_has(c
     pass
 
 
-@pytest.mark.skip
 def test_does_nothing_when_product_does_not_exists(client):
     # Arrange (?)
     # Act (substract any quantity to any sku)
