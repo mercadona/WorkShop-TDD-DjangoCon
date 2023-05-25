420849fdcffdfd1c6c217bc1f2921a150b869d59
Luis Laguna <lulaguna@mercadona.es>
2023-05-25 12:22:17 +0200

    feat: Add helper for retrieve the data from the request

--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,10 @@ from django.http import JsonResponse
 from .models import CartLine
 
 
+def _get_data_from_request(request):
+    return request.POST.dict()
+
+
 def cart_detail(request):
     lines = CartLine.objects.all()
     return JsonResponse({"lines": [line.to_dict() for line in lines]})
